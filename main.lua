4e6327472b6fc63e00248bd749d386bead926748
celestialvesselfr <xqtn@proton.me>
2025-10-05 06:09:00 +1100

    another quick fix

--- main.lua
+++ main.lua
@@ -1911,7 +1911,6 @@ function UILibrary:CreateDropdown(tab, name, options, default, callback)
 	end)
 	
 	-- return dropdown object with refresh method
-{{ ... }}
 	local dropdownObject = {
 		Container = container,
 		Options = options,
